fd71ce81c21733e800e3367258ae096032d7f4cc
Gregorian1489 <santijago@mail.ru>
2022-11-30 19:58:17 +0400

    add task41

---
+++ task41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел");
+string line = Console.ReadLine();
+string [] array = line.Split(' ');
+int [] arr = Array.ConvertAll(array, int.Parse);
+int sum = 0;
+
+for (int i = 0; i < arr.Length;i++)
+{
+    if (arr[i]<0)
+    sum++;
+}
+Console.WriteLine($"Вы ввели {sum} отрицательных чисел");
+    
---
+++ task43/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Даны уравнения:");
+Console.WriteLine("y = k1 * x + b1");
+Console.WriteLine("y = k2 * x + b2");
+Console.WriteLine("Введите b1 k1 b2 k2 через пробел");
+
+string line = Console.ReadLine();
+string [] array = line.Split(' ');
+double [] arr = Array.ConvertAll(array, double.Parse);
+if (arr[1] == arr[3]) Console.WriteLine("Прямые параллельны");
+else
+{
+    double x = ((arr[2]-arr[0] )/(arr[1]-arr[3]));
+    double y = arr[1]*x + arr[0];
+    Console.WriteLine($"Координаты точки пересечения: {Math.Round(x,2)} {Math.Round(y,2)}");
+}
